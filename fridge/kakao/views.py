bd5867a4e81a73358194d607fb5dff15bf86aaef
ch02w <scho020224@gmail.com>
2024-07-06 03:02:03 +0900

    Modify: 로그인 후 users 테이블에 저장

--- fridge/kakao/views.py
+++ fridge/kakao/views.py
@@ -1,6 +1,6 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from .models import User
+from .models import Users
 import json
 
 @csrf_exempt
@@ -9,7 +9,7 @@ def save_user(request):
         data = json.loads(request.body)
         user_id = data.get('kakao_id')
         nickname = data.get('nickname')
-        user, created = User.objects.get_or_create(user_id=user_id, defaults={'nickname': nickname})
+        user, created = Users.objects.get_or_create(user_id=user_id, defaults={'nickname': nickname})
         if not created:
             user.nickname = nickname
             user.save()
